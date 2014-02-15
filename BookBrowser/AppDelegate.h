1bf6ae8b24d91220393ff97cfd4b2c974bd7f7df
Sebastian Varela <sebastian@svfnet.com>
2014-02-15 18:20:53 +0100

    Añado el proyecto vacío de xcode con los dos storyboards.

---
+++ BookBrowser/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BookBrowser
+//
+//  Created by Sebastián Varela on 15/02/14.
+//  Copyright (c) 2014 Sebastián Varela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BookBrowser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BookBrowser
+//
+//  Created by Sebastián Varela on 15/02/14.
+//  Copyright (c) 2014 Sebastián Varela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
